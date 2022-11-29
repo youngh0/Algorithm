693c07f43a5a3e5b7d06e469852f0052eeb3db4e
Youngho <cyh6099@gmail.com>
2022-11-29 16:32:41 +0900

    백준 카드2 - Queue

---
+++ Q/boj_2164.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+n = int(input())
+
+arr = [i for i in range(1,n+1)]
+q = deque(arr)
+
+while len(q) > 1:
+    q.popleft()
+
+    q.append(q.popleft())
+
+print(q[0])
